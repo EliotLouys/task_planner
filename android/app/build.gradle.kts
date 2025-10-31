c6d96708a953c2984bd53d762bd4b1df5796a3fc
EliotLouys <eliot.louys@grenoble-inp.org>
2025-10-31 14:53:29 +0100

    Change the project sdk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.zbeub_task_plan"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
